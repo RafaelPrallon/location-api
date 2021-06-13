b98c4ec0746b96e3a117c722e49be07e8eb2035f
RafaelPrallon <rafaelprallon@gmail.com>
2021-06-13 00:02:15 -0300

    master initial setup and add authentication

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,14 @@
+# Class to handle api sessions
+class SessionsController < Devise::SessionsController
+  respond_to :json
+
+  private
+
+  def respond_with(resource, _opts = {})
+    render json: resource
+  end
+
+  def respond_to_on_destroy
+    head :no_content
+  end
+end
---
+++ app/models/user.rb
@@ -0,0 +1,13 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  include Devise::JWT::RevocationStrategies::JTIMatcher
+
+  devise :registerable,
+         :database_authenticatable,
+         :jwt_authenticatable,
+         jwt_revocation_strategy: self
+
+  validates :email, uniqueness: true
+  validates :username, uniqueness: true
+end
